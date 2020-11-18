9e8fd4734951b98c47d97e5579cb31699da913fa
yanshiyin <498794327@qq.com>
2020-11-18 16:34:37 +0800

    rmvpackages

--- src/main.js
+++ src/main.js
@@ -5,16 +5,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
-import Vtip from 'vtip'
-import 'vtip/lib/index.min.css'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
 Vue.component('v-chart',ECharts)
 Vue.use(VueCodemirror,{})
-Vue.use(Vtip.directive)
+// Vue.use(Vtip.directive)
 
-Vue.prototype.$tip = Vtip.tip
+// Vue.prototype.$tip = Vtip.tip
 
 new Vue({
   router,
